fb82e05f4c567829955c26a8a46b8f9b431b52b9
s28518 <s28518@pjwstk.edu.pl>
2024-04-07 20:23:44 +0200

    Refactoring + testing

--- zadanie/LegacyApp.Tests/UserServiceTests.cs
+++ zadanie/LegacyApp.Tests/UserServiceTests.cs
@@ -3,7 +3,7 @@ namespace LegacyApp.Tests;
 public class UserServiceTests
 {
     [Fact]
-    public void AddUser_ReturnsFalseWhenFirstNameIsEmpty()
+    public void AddUser_Returns_False_When_First_Name_Is_Empty()
     {
         // Arrange
         var userService = new UserService();
@@ -24,7 +24,7 @@ public class UserServiceTests
     }
     
     [Fact]
-    public void AddUser_ThrowsExceptionWhenClientDoesNotExist()
+    public void AddUser_Throws_Exception_When_Client_Does_Not_Exist()
     {
         // Arrange
         var userService = new UserService();
@@ -42,5 +42,132 @@ public class UserServiceTests
         // Assert
         
         Assert.Throws<ArgumentException>(action);
+    }
+    
+    [Fact]
+    public void AddUser_Returns_False_When_Email_Without_At_And_Dot()
+    {
+        // Arrange
+        var userService = new UserService();
+        
+        // Act
+
+        var result = userService.AddUser(
+            "Jan",
+            "Kowalski",
+            "kowalskikowalcom",
+            DateTime.Parse("2000-01-01"),
+            2
+        );
+        
+        // Assert
+        
+        Assert.False(result);
+    }
+    
+    [Fact]
+    public void AddUser_ReturnsFalseWhenAgeUnder21()
+    {
+        var userService = new UserService();
+        var result = userService.AddUser(
+            "Jan",
+            "Kowalski",
+            "kowalski@kowal.com",
+            DateTime.Parse("2020-01-01"),
+            2
+        );
+        Assert.False(result);
+    }
+    
+    [Fact]
+    public void AddUser__Returns_False_When_User_Has_CreditLimit_Below_500()
+    {
+        var userService = new UserService();
+        var addResult = userService.AddUser(
+            "Jan",
+            "Kowalski",
+            "jan@kowal.com",
+            DateTime.Parse("1982-03-21"),
+            1);
+        Assert.False(addResult);
+    }
+    
+    [Fact]
+    public void AddUser_Returns_True_When_User_Is_Important_Client()
+    {
+        var userService = new UserService();
+        var addResult = userService.AddUser(
+            "John",
+            "Smith",
+            "smith@gmail.pl",
+            DateTime.Parse("1980-05-01"),
+            3);
+        Assert.True(addResult);
+    }
+
+    [Fact]
+    public void AddUser_Returns_True_When_User_Is_Very_Important_Client()
+    {
+        var userService = new UserService();
+        var addResult = userService.AddUser(
+            "Jan",
+            "Malewski",
+            "malewski@gmail.pl",
+            DateTime.Parse("1990-10-25"),
+            2);
+    
+        Assert.True(addResult);
+    }
+    
+    [Fact]
+    public void AddUser_Returns_True_When_User_Is_NormalClient_And_CreditLimit_Is_Above_500()
+    {
+        var userService = new UserService();
+        var addResult = userService.AddUser(
+            "Jan",
+            "Kwiatkowski",
+            "kwiatkowski@wp.pl",
+            DateTime.Parse("1990-10-25"),
+            5);
+        Assert.True(addResult);
+    }
+    
+    [Fact]
+    public void AddUser_Returns_True_When_User_Has_CreditLimit_Above_500()
+    {
+        var userService = new UserService();
+        var addResult = userService.AddUser(
+            "John",
+            "Doe",
+            "doe@gmail.pl",
+            DateTime.Parse("1982-03-21"),
+            4);
+        Assert.True(addResult);
+    }
+
+    [Fact]
+    public void AddUser_Returns_False_When_LastName_Is_Missing()
+    {
+        var userService = new UserService();
+        var addResult = userService.AddUser(
+            "John",
+            "",
+            "doe@gmail.com",
+            DateTime.Parse("1982-03-21"),
+            1);
+        Assert.False(addResult);
+    }
+    
+    [Fact]
+    public void AddUser_Returns_False_When_FirstName_Is_Missing()
+    {
+        var userService = new UserService();
+        var addResult = userService.AddUser(
+            "", 
+            "Doe",
+            "doe@gmail.com",
+            DateTime.Parse("1982-03-21"),
+            1);
+        Assert.False(addResult);
     }
 }
--- zadanie/LegacyApp/UserService.cs
+++ zadanie/LegacyApp/UserService.cs
@@ -4,30 +4,71 @@ namespace LegacyApp
 {
     public class UserService
     {
+        private readonly IClientRepository _clientRepository;
+        private ICreditLimitService _creditService;
+        
+        [Obsolete]
+        public UserService()
+        {
+            _clientRepository = new ClientRepository();
+            _creditService = new UserCreditService();
+        }
+
+        public UserService(IClientRepository clientRepository, ICreditLimitService creditService)
+        {
+            _clientRepository = clientRepository;
+            _creditService = creditService;
+        }
+        
         public bool AddUser(string firstName, string lastName, string email, DateTime dateOfBirth, int clientId)
         {
-            if (string.IsNullOrEmpty(firstName) || string.IsNullOrEmpty(lastName))
+            if (!IsValidFirstName(firstName) || !IsValidLastName(lastName) || !IsValidEmail(email) || !IsUserOldEnough(dateOfBirth))
             {
                 return false;
             }
 
-            if (!email.Contains("@") && !email.Contains("."))
+            var client = _clientRepository.GetById(clientId);
+
+            var user = CreateUser(firstName, lastName, email, dateOfBirth, client);
+
+            if (!IsValidCreditLimit(client, user))
             {
                 return false;
             }
 
+            SaveUser(user);
+
+            return true;
+        }
+        
+        private bool IsValidFirstName(string firstName)
+        {
+            return !string.IsNullOrEmpty(firstName);
+        }
+
+        private bool IsValidLastName(string lastName)
+        {
+            return !string.IsNullOrEmpty(lastName);
+        }
+
+        private bool IsValidEmail(string email)
+        {
+            return !string.IsNullOrEmpty(email) && (email.Contains("@") && email.Contains("."));
+        }
+
+        private bool IsUserOldEnough(DateTime dateOfBirth)
+        {
             var now = DateTime.Now;
             int age = now.Year - dateOfBirth.Year;
-            if (now.Month < dateOfBirth.Month || (now.Month == dateOfBirth.Month && now.Day < dateOfBirth.Day)) age--;
-
-            if (age < 21)
+            if (now.Month < dateOfBirth.Month || (now.Month == dateOfBirth.Month && now.Day < dateOfBirth.Day))
             {
-                return false;
+                age--;
             }
+            return age >= 21;
+        }
 
-            var clientRepository = new ClientRepository();
-            var client = clientRepository.GetById(clientId);
-
+        private User CreateUser(string firstName, string lastName, string email, DateTime dateOfBirth, Client client)
+        {
             var user = new User
             {
                 Client = client,
@@ -41,32 +82,24 @@ namespace LegacyApp
             {
                 user.HasCreditLimit = false;
             }
-            else if (client.Type == "ImportantClient")
-            {
-                using (var userCreditService = new UserCreditService())
-                {
-                    int creditLimit = userCreditService.GetCreditLimit(user.LastName, user.DateOfBirth);
-                    creditLimit = creditLimit * 2;
-                    user.CreditLimit = creditLimit;
-                }
-            }
             else
             {
+                int creditLimit = _creditService.GetCreditLimit(user.LastName, user.DateOfBirth);
+                user.CreditLimit = client.Type == "ImportantClient" ? creditLimit * 2 : creditLimit;
                 user.HasCreditLimit = true;
-                using (var userCreditService = new UserCreditService())
-                {
-                    int creditLimit = userCreditService.GetCreditLimit(user.LastName, user.DateOfBirth);
-                    user.CreditLimit = creditLimit;
-                }
             }
 
-            if (user.HasCreditLimit && user.CreditLimit < 500)
-            {
-                return false;
-            }
+            return user;
+        }
 
+        private bool IsValidCreditLimit(Client client, User user)
+        {
+            return !user.HasCreditLimit || user.CreditLimit >= 500;
+        }
+
+        private void SaveUser(User user)
+        {
             UserDataAccess.AddUser(user);
-            return true;
         }
     }
 }
